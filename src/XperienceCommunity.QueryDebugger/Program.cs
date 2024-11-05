77d42ce459f5d472b17167c1997ea8309bd53391
Sean G. Wright <sean@seangwright.me>
2024-11-05 14:43:01 -0500

    feat(QueryDebugger): better example queries

--- src/XperienceCommunity.QueryDebugger/Program.cs
+++ src/XperienceCommunity.QueryDebugger/Program.cs
@@ -1,4 +1,5 @@
-﻿using CMS.ContentEngine;
+﻿
+using CMS.ContentEngine;
 using CMS.Core;
 using CMS.DataEngine;
 using CMS.Membership;
@@ -26,9 +27,46 @@ CMSApplication.Init();
 var profiler = MiniProfiler.StartNew();
 
 var exectutor = app.Services.GetRequiredService<IContentQueryExecutor>();
-var b = new ContentItemQueryBuilder()
-    .ForContentTypes(q => q.OfContentType(Coffee.CONTENT_TYPE_NAME));
-await profiler.PrintQuery(Coffee.CONTENT_TYPE_NAME, () => exectutor.GetResult(b, i => i.ContentItemID), new() { PopulateCache = false });
+
+await profiler.PrintQuery(
+    $"{Coffee.CONTENT_TYPE_NAME} default query",
+    () =>
+    {
+        var b = new ContentItemQueryBuilder()
+            .ForContentTypes(q => q.OfContentType(Coffee.CONTENT_TYPE_NAME));
+        return exectutor.GetResult(b, i => i.ContentItemID);
+    });
+
+await profiler.PrintQuery(
+    $"{nameof(ISEOFields)} with no cache",
+    () =>
+    {
+        var b = new ContentItemQueryBuilder()
+            .ForContentTypes(q => q.OfReusableSchema(ISEOFields.REUSABLE_FIELD_SCHEMA_NAME));
+        return exectutor.GetResult(b, i => i.ContentItemID);
+    },
+    new() { PopulateCache = false });
+
+await profiler.PrintQuery(
+    "Coffee by taxonomy with image",
+    () =>
+    {
+        var b = new ContentItemQueryBuilder()
+            .ForContentTypes(q => q
+                .OfContentType(Coffee.CONTENT_TYPE_NAME)
+                .WithContentTypeFields()
+                .WithLinkedItems(1))
+            .Parameters(q => q
+                .Where(w => w.WhereContainsTags(nameof(Coffee.CoffeeProcessing), [new Guid("9891389f-e404-476a-8eb8-55adea059d51")]))
+                .Columns(nameof(Coffee.SystemFields.ContentItemID), nameof(Coffee.CoffeeProcessing), nameof(Coffee.ProductFieldsName), nameof(Coffee.ProductFieldsImage))
+                .OrderBy(nameof(Coffee.ProductFieldsName))
+                .TopN(5)
+                .IncludeTotalCount());
+        return exectutor.GetResult(b, i => i.ContentItemID);
+    });
 
 var userProvider = Service.Resolve<IInfoProvider<UserInfo>>();
-await profiler.PrintQuery(UserInfo.OBJECT_TYPE, () => userProvider.Get().GetEnumerableTypedResultAsync(), new() { Verbose = true });
+await profiler.PrintQuery(
+    $"{UserInfo.OBJECT_TYPE} verbose",
+    () => userProvider.Get().GetEnumerableTypedResultAsync(),
+    new() { Verbose = true });
